6c495154ba60dcd307644b7c415229a2081f45ee
parthvijay123 <parth22vijay22@gmail.com>
2025-03-30 16:08:59 +0530

    binaries removal script

--- scripts/rm-bins.sh
+++ scripts/rm-bins.sh
@@ -2,3 +2,12 @@
 
 # Remove all the binary files (usually made after gcc) present inside current directory
 # Refer "man find" for more information, you can also use modern tool like fd instead of find
+
+for file in *; do
+    if [ -f "$file" ]; then
+        if file "$file" | grep -q "Mach-O"; then
+            echo "Removing: $file"
+            rm -v "$file"
+        fi
+    fi
+done
